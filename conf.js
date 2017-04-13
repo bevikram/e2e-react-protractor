50f585672159e128f58530b7c7cd6635e8be905d
Vikram Belde <belvikram@gmail.com>
2017-04-13 11:46:50 -0500

    Added html reported plugin

--- conf.js
+++ conf.js
@@ -1,7 +1,12 @@
 'use strict';
 const input = require('./variables');
-
 // React Web testing
+
+//Clean UI with pie Diagram 
+var today = new Date().toISOString()
+var date = today.split('T')[0]
+var time = today.split('T')[1].slice(0, -1)
+
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: input.baseUrl,
@@ -12,12 +17,36 @@ exports.config = {
   // multiCapabilities: [{
   //   'browserName': 'firefox'
   // }, {
-  //   'browserName': 'phantomjs'
+  //   'browserName': 'chrome'
   // }],
   suites: {
     login: ['./specs/login.js'],
     create_customer: ['./specs/createCustomer.js'],
-    logout: ['./specs/logout.js']
+    checkCustCreation: ['./specs/checkCustCreation.js']
+  },
+  resultJsonOutputFile: './result.json',
+  onComplete: function() {
+    var browserName,
+      browserVersion;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function(caps) {
+      browserName = caps.get('browserName');
+      browserVersion = caps.get('version');
+
+      var HTMLReport = require('protractor-html-reporter');
+
+      var testConfig = {
+        reportTitle: 'NetSense ',
+        outputPath: './reports/' + date + '/' + time + '/htmlReports',
+        screenshotPath: './reports/htmlReports/screenshots',
+        testBrowser: browserName,
+        browserVersion: browserVersion,
+        modifiedSuiteName: false,
+        screenshotsOnlyOnFailure: true
+      };
+      new HTMLReport().from('./reports/' + date + '/' + time + '/xmlResults/' + 'xmlTestResults.xml', testConfig);
+    });
   },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
@@ -25,6 +54,12 @@ exports.config = {
     var height = 1200;
     browser.driver.manage().window().setSize(width, height);
     require('babel-register');
+    var jasmineReporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+      consolidateAll: true,
+      savePath: './reports/' + date + '/' + time + '/xmlResults',
+      filePrefix: 'xmlTestResults'
+    }));
   },
   getPageTimeout: 50000,
   allScriptsTimeout: 50000,
